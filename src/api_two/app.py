6d64c1264db7002041ac2808d99f624f261676cf
alfmorais <alfredneto@hotmail.com>
2022-02-07 21:49:57 -0300

    implementação do endpoint post e get

--- src/api_two/app.py
+++ src/api_two/app.py
@@ -5,13 +5,29 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
-class Student(Resource):
-    def get(self, name):
-        return {"student": name}
+class Items(Resource):
+    def get(self, name: str):
+
+        for item in items:
+            item_name = item["name"]
+            if item_name == name:
+                return item
+        return {"message": f"The item {name} was not found!"}, 404
+
+    def post(self, name):
+        item = {"name": name, "price": 12.00}
+        items.append(item)
+        return item, 201
+
+    def put(self, name):
+        pass
+
+    def delete(self, name):
+        pass
 
 
-api.add_resource(Student, "/student/<string:name>")
-# url example: http://127.0.0.1:5000/student/joaquim
+api.add_resource(Items, "/items/<string:name>")
 
 app.run(port=5000)
