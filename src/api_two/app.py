e740a78f6dafcf8a02eed38ee07637d6a17824f8
alfmorais <alfredneto@hotmail.com>
2022-02-08 21:54:04 -0300

    implementando o JWT

--- src/api_two/app.py
+++ src/api_two/app.py
@@ -1,33 +1,44 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request
+from flask_jwt import JWT, jwt_required
+from flask_restful import Api, Resource
+
+from security import authenticate, identity
 
 
 app = Flask(__name__)
+app.secret_key = "alfredo"
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
-class Items(Resource):
-    def get(self, name: str):
 
-        for item in items:
-            item_name = item["name"]
-            if item_name == name:
-                return item
-        return {"message": f"The item {name} was not found!"}, 404
+class Items(Resource):
+    @jwt_required()
+    def get(self, name: str):
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        return {"message": item}, 200 if item else 404
 
     def post(self, name):
-        item = {"name": name, "price": 12.00}
+        condition = next(filter(lambda x: x["name"] == name, items), None)
+
+        if condition is not None:
+            return {"message": f"An item with name {name} already exists."}, 400
+
+        data = request.get_json()
+        price = data.get("price", False)
+        item = {"name": name, "price": price}
         items.append(item)
         return item, 201
 
-    def put(self, name):
-        pass
 
-    def delete(self, name):
-        pass
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}
 
 
 api.add_resource(Items, "/items/<string:name>")
+api.add_resource(ItemList, "/items")
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
